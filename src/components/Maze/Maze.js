a6f4de00a7cc62e82f218756f40ba3c001e63302
AbdiLghouse <abdi@lghouse.co.uk>
2023-04-20 22:37:59 +0100

    Main game completed

--- src/components/Maze/Maze.js
+++ src/components/Maze/Maze.js
@@ -1,10 +1,10 @@
-import Wealth from './Wealth'
+import Room from './Room'
+import { useState, useEffect } from 'react'
 
-export default function Maze({ gameState, setGameState }) {
+export default function Maze({ wealth, currentRoom, maze }) {
+
     return (
-        <div className="relative flex flex-col items-center justify-center h-[500px] w-[800px] bg-[#546E7A]">
-            <Wealth wealth={gameState.wealth} />
-            <h1>Maze</h1>
-        </div>
+        <Room wealth={wealth} room={maze[currentRoom]} />
     )
 }
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState, useEffect } from 'react'
 
-import { map } from '../../data/maze.json'
+import Map from '../../data/maze.json'
 import MainMenu from '../components/MainMenu/MainMenu.js'
 import Maze from '../components/Maze/Maze.js'
 import End from '../components/End/End.js'
@@ -11,24 +11,25 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     const [pageType, setPageType] = useState('menu')
-
-    const [gameState, setGameState] = useState({
-        wealth: 0,
-        maze: map,
-    })
+    const [wealth, setWealth] = useState(0)
+    const [maze, setMaze] = useState(Map)
+    const [currentRoom, setCurrentRoom] = useState(getRandomInt(maze.length - 1))
 
     return (
         <main className="flex items-center justify-center p-24">
             {pageType == 'menu' && <MainMenu setPageType={setPageType} />}
             {pageType == 'maze' && (
                 <Maze
-                    setGameState={setGameState}
-                    gameState={gameState}
-                    wealth={gameState.wealth}
-                    maze={gameState.maze}
+                    wealth={wealth}
+                    currentRoom={currentRoom}
+                    maze={maze}
                 />
             )}
-            {pageType == 'end' && <End setGameState={setGameState} gameState={gameState} wealth={gameState.wealth} />}
+            {pageType == 'end' && <End wealth={wealth} />}
         </main>
     )
+}
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
 }
